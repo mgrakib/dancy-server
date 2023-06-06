f1065441bbc848e896955fbee7c195bb36bd3496
toma570 <itextoma@gmail.com>
2023-06-06 22:44:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+	res.send("Bristo Server running...");
+});
+
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
